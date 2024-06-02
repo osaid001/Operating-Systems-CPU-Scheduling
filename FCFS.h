2363b6ddff514e3e08b7972300cc5544ee225afe
osaid amjad <137379076+osaid001@users.noreply.github.com>
2024-06-02 22:53:39 +0500

    Update FCFS.h

--- FCFS.h
+++ FCFS.h
@@ -7,6 +7,8 @@ private:
     processfcfs* head;
     int num_processes;
 
+    processfcfs* findProcessById(int pid);
+
 public:
     scheduler();
     void addProcess(int pid, int arrival, int burst);
@@ -18,5 +20,11 @@ public:
     void getWaitingTimes(int* waiting_times);
     void getProcessIdsInCompletionOrder(int* process_ids);
 
+    int get_Arrival_Time(int pid);
+    int get_Burst_Time(int pid);
+
+    int* getProcessRunningTimeline(int& total_time);
+
     ~scheduler();
 };
+
